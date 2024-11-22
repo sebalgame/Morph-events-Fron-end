95f1398af9d81d0be348cd47847309f2d5bd7ee7
sebalgame <105014983+sebalgame@users.noreply.github.com>
2024-11-22 17:36:12 -0500

    Add files via upload

---
+++ src/app/auth.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken();
+
+    const excludedUrls = ['/authorities/list', '/users/create', '/users/login', '/clients/create'];
+
+    if (excludedUrls.some(url => req.url.includes(url))) {
+      return next.handle(req);
+    }
+
+    if (token) {
+      const authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return next.handle(authReq);
+    }
+
+    return next.handle(req);
+  }
+}
